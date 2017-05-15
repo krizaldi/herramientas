f5fd4dd2196fb95619e2722a3515ed853950307c
Cesar <Cesar@LAPTOP-GN083H4I>
2017-05-15 17:04:29 -0500

    bases

---
+++ Herramientas/src/main/java/mx/edu/uacm/herramientas/domain/Administrador.java
@@ -0,0 +1,32 @@
+package mx.edu.uacm.herramientas.domain;//
+//
+//
+//
+//  @ Project : Herramientas de software
+//  @ File Name : Busqueda.java
+//  @ Date : 02/10/2016
+//  @ Author : Equipo 1
+//
+//
+
+
+
+
+public class Administrador extends Persona {
+	private String nombre;
+	private String contrasena;
+
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getContrasena() {
+		return contrasena;
+	}
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
+	}
+
+}
---
+++ Herramientas/src/main/java/mx/edu/uacm/herramientas/domain/Persona.java
@@ -0,0 +1,75 @@
+package mx.edu.uacm.herramientas.domain;
+
+public class Persona {
+	private String nombre;
+	private String apellidoPaterno;
+	private String apellidoMaterno;
+	private String email;
+	private String telefono;
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	/**
+	 * @return the apellidoPaterno
+	 */
+	public String getApellidoPaterno() {
+		return apellidoPaterno;
+	}
+	/**
+	 * @param apellidoPaterno the apellidoPaterno to set
+	 */
+	public void setApellidoPaterno(String apellidoPaterno) {
+		this.apellidoPaterno = apellidoPaterno;
+	}
+	/**
+	 * @return the apellidoMaterno
+	 */
+	public String getApellidoMaterno() {
+		return apellidoMaterno;
+	}
+	/**
+	 * @param apellidoMaterno the apellidoMaterno to set
+	 */
+	public void setApellidoMaterno(String apellidoMaterno) {
+		this.apellidoMaterno = apellidoMaterno;
+	}
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	/**
+	 * @return the telefono
+	 */
+	public String getTelefono() {
+		return telefono;
+	}
+	/**
+	 * @param telefono the telefono to set
+	 */
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	
+	
+	
+	
+
+}
+
